fca629aafeed71c62c6c1aa529fa9b9faf49c742
houxiaoxuan <1159991343@qq.com>
2020-10-10 19:36:16 +0800

    1.callback 2.eventHandler

--- CSharpSyntax_Demo/Program.cs
+++ CSharpSyntax_Demo/Program.cs
@@ -2,9 +2,12 @@
 //#undef DEBUG
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CSharpSyntax_Demo
@@ -37,13 +40,104 @@ namespace CSharpSyntax_Demo
     }
 
     #endregion
+    #region 委托回调
+    class DelegateTest
+    {
+        public delegate void Workdone(bool IsTrue);
+        public void ToWork(Workdone workdone)
+        {
+            workdone(true);
+        }
+
+        public void WorkDoneHandler(bool istrue)
+        {
+            if(istrue)
+                Console.WriteLine("Work has done!");
+        }
+    }
+    //接口回调
+    class MyBack : IBack
+    {
+        public void Run(bool isFalse)
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+    public class Controller
+    {
+        public IBack Back = null;
+        public Controller(IBack back)
+        {
+            Back = back;
+        }
+        public void Start()
+        {
+            Console.WriteLine("敲键盘任意键就显示当前的时间，直到按ESC退出...."); 
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape) { Back.Run(true); }
+        }
+    }
+    public interface IBack
+    {
+        void Run(bool isFalse);
+    }
+    //事件
+    
+    public class MyEvent
+    {
+        public delegate void EventHandler(bool isFalse);
+        public event EventHandler eventHandler;
+
+        public void OnRaise()
+        {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            eventHandler(true);
+        }
+    }
+    public class MyEventReceiver
+    {
+        public BackgroundWorker Worker;
+        public void HostHandleEvent(bool isFalse)
+        {
+            Worker.RunWorkerAsync();
+        }
+        public MyEventReceiver(MyEvent eventArgs,BackgroundWorker worker)
+        {
+            Worker = worker;
+            eventArgs.eventHandler += new MyEvent.EventHandler(HostHandleEvent);
+        }
+    }
+    #endregion
+
     class Program
     {
+        private static DelegateTest call = new DelegateTest();
         static void Main(string[] args)
         {
+            DelegateTest.Workdone test = new DelegateTest.Workdone(call.WorkDoneHandler);
+            call.ToWork(test);
+            //Controller cc = new Controller(new MyBack());
+            //cc.Start();
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            MyEvent myEvent = new MyEvent();
+            //myEvent.eventHandler += new MyEvent.EventHandler(call.WorkDoneHandler);
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.DoWork += new DoWorkEventHandler(BackgroundWorkerDowork);
+            MyEventReceiver myEventReceiver= new MyEventReceiver(myEvent, worker);
+            //myEvent.eventHandler += new MyEvent.EventHandler(myEventReceiver.HostHandleEvent);
+            myEvent.OnRaise();
+            Console.ReadKey();
+
             Ope oa = new Ope(1,2);
             Ope ob = new Ope(3, 4);
             Ope v = oa + ob;
         }
+        private static void BackgroundWorkerDowork(object sender, DoWorkEventArgs e)
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine("has received");
+            call.WorkDoneHandler(true);
+        }
     }
 }
+
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public static BackgroundWorker Worker;
+        
+        private static Form form2;
+        private static AssistClass assistClass = new AssistClass();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "InitSuccess";
+            Worker = new BackgroundWorker();
+            Worker.DoWork += new DoWorkEventHandler(DoWork);
+            Worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(WorkerCompleted);
+            if (!Worker.IsBusy) 
+                Worker.RunWorkerAsync();
+        }
+
+        private void WorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Thread.Sleep(2000);
+            textBox1.Text = "WorkerCompleted";
+        }
+        private delegate void EnableButtonCallBack(bool IsEnable);
+        private delegate void ChangeTextCallBack();
+        public AssistClass.AssistCallBack assistCall = new AssistClass.AssistCallBack(assistClass.Run);
+        private void DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+            //EnableButtonCallBack btnCall = new EnableButtonCallBack();
+            while (AssistClass.count< 1000)
+            {
+                
+                bool ret = assistClass.Add(assistCall);
+                EnableBtn();
+                if(!ret)
+                    AssistClass.resetEvent.WaitOne();
+                textBox1.BeginInvoke(new ChangeTextCallBack(ChangeText));//可以改变控件内容
+            }
+            //ChangeText();//不会改变控件内容
+            //textBox1.Invoke(new EnableButtonCallBack(ChangeText));//可以改变控件内容
+            //Thread.Sleep(1000);
+        }
+        private void EnableBtn()
+        {
+            textBox1.BeginInvoke(new ChangeTextCallBack(IsEnable));
+        }
+        private void IsEnable()
+        {
+            textBox1.Enabled = !AssistClass.isPausing;
+        }
+        private void ChangeText()
+        {
+            textBox1.Text = AssistClass.count.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!AssistClass.isPausing)
+                AssistClass.Pause();
+            else
+                AssistClass.Resume();
+        }
+    }
+    public class AssistClass
+    {
+        public static AutoResetEvent resetEvent = new AutoResetEvent(true);
+        public static int count = 0;
+        public static bool isPausing = false;
+        public delegate int AssistCallBack();
+        public bool Add(AssistCallBack assistCall)
+        {
+            if (Run() == 0)
+                return false;
+            else
+                return true;
+        }
+        //public int Count { get { return count; }set { count = value; } }
+        public static void Pause()
+        {
+            resetEvent.Reset();
+            isPausing = true;
+        }
+        public static void Resume()
+        {
+            resetEvent.Set();
+            isPausing = false;
+        }
+        public int Run()
+        {
+            Thread.Sleep(2000);
+            if (!isPausing)
+                return Interlocked.Increment(ref count);
+            else
+                return 0;
+        }
+    }
+}
